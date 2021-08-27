171bbdba0dc1738f61cb8e167ea83b2979e6ccf9
octosport <contact@octosport.io>
2021-08-27 16:43:59 +0100

    [MAINT] update package

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_packages
 
 DISTNAME = "octopy"
 VERSION = "1.0.0"
-DESCRIPTION = """Octopy is a compagnon Python library for octosport.io. The library offers tools for football (soccer) analytics."""
-LONG_DESCRIPTION = """Octopy is a compagnon Python library for octosport.io. The library offers tools for football (soccer) analytics."""
+DESCRIPTION = """Octopy is a companion Python library for octosport.io. The library offers tools for football (soccer) analytics."""
+LONG_DESCRIPTION = """Octopy is a companion Python library for octosport.io. The library offers tools for football (soccer) analytics."""
 AUTHOR = "octosport.io"
 AUTHOR_EMAIL = "contact@octosport.io"
 URL = "https://github.com/octosport/octopy"
 LICENSE = "Apache License, Version 2.0"
 
 
-REQUIREMENTS = ["pandas>=1.1.3", "scipy>=1.5.2", "scikit-learn>=0.23.2"]
+REQUIREMENTS = ["pandas>=1.1.3", "scipy>=1.5.2", "scikit-learn>=0.23.2", "jax>=0.2.19"]
 
 if __name__ == "__main__":
     setup(
